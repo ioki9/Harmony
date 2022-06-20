23ebcd4d4ea8e52bd7f68a8d4d77b047df805f2f
Laritello <larik97@gmail.com>
2022-06-20 19:49:39 +0300

    Blank app added

--- src/Harmony.UI/UIModule.cs
+++ src/Harmony.UI/UIModule.cs
@@ -1,12 +1,22 @@
-﻿using Prism.Ioc;
+﻿using Harmony.Common;
+using Harmony.UI.Views;
+using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 
 namespace Harmony.UI
 {
     public class UIModule : IModule
     {
-        public void OnInitialized(IContainerProvider containerProvider) {}
+        public void OnInitialized(IContainerProvider containerProvider) 
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RequestNavigate(RegionNames.ContentRegion, nameof(MainView));
+        }
 
-        public void RegisterTypes(IContainerRegistry containerRegistry) {}
+        public void RegisterTypes(IContainerRegistry containerRegistry) 
+        {
+            containerRegistry.RegisterForNavigation<MainView>();
+        }
     }
 }
