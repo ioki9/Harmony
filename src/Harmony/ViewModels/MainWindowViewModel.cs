7297a9777198e6b6f889475f1ef91c69ea144cb9
Laritello <larik97@gmail.com>
2022-06-20 19:45:25 +0300

    Imported prism modules to the main project

--- src/Harmony/ViewModels/MainWindowViewModel.cs
+++ src/Harmony/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace Harmony.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Harmony";
         public string Title
         {
             get { return _title; }
