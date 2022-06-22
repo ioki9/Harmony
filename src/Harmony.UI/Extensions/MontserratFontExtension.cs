928997d08533da236baa1c18143f0215b3baf243
Laritello <larik97@gmail.com>
2022-06-22 16:42:19 +0300

    Added title to the title bar

---
+++ src/Harmony.UI/Extensions/MontserratFontExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Harmony.UI.Extensions
+{
+    [MarkupExtensionReturnType(typeof(FontFamily))]
+    public class MontserratFontExtension : MarkupExtension
+    {
+        private static readonly Lazy<FontFamily> _montserrat =
+            new(() => new FontFamily(new Uri("pack://application:,,,/Harmony.UI;component/Resources/Montserrat/"), "./#Montserrat"));
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => _montserrat.Value;
+    }
+}
