1c9d7f2f6153b55ff7925a6f9fae9fb6147454eb
Laritello <larik97@gmail.com>
2022-06-22 13:05:12 +0300

    Poppins font added

---
+++ src/Harmony.UI/Extensions/PoppinsFontExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Harmony.UI.Extensions
+{
+    [MarkupExtensionReturnType(typeof(FontFamily))]
+    public class PoppinsFontExtension : MarkupExtension
+    {
+        private static readonly Lazy<FontFamily> _poppins = 
+            new(() => new FontFamily(new Uri("pack://application:,,,/Harmony.UI;component/Resources/Poppins/"), "./#Poppins"));
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => _poppins.Value;
+    }
+}
